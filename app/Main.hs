fa603786bb97bdd1fb97f35dc8f96c20f4efda8b
De3up <de3up1@gmail.com>
2018-05-24 21:29:49 +0300

    Game black board

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
-module Main where
+module Main  where
 
-import Lib
 
+import Graphics.Gloss.Interface.Pure.Game
 main :: IO ()
-main = someFunc
+main =
+  play display bgColor fps initialWorld renderWorld handleWorld updateWorld
+  where
+    display = InWindow "Game of Tron" winSize winOffset
+    bgColor = black
+    fps = 60
+    initialWorld = ()
+    renderWorld w = blank
+    handleWorld _ w = w
+    updateWorld _ w = w
+    winSize = ( 800, 800 )
+    winOffset = ( 100, 100 )
