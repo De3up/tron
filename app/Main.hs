5f06e8b66b163f1825fb5870b77d505c229d4572
De3up <de3up1@gmail.com>
2018-05-30 17:40:38 +0300

    Correction code in Main.s

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,4 @@
-module Main  where
+module Main where
 
 
 import  Graphics.Gloss.Interface.Pure.Game
@@ -7,12 +7,12 @@ main :: IO ()
 main =
     play display bgColor fps initialWorld renderWorld handleWorld updateWorld
   where
-    display = InWindow "Game of Tron" winSize winOffset
+    display = InWindow "Game of Trons" winSize winOffset
     bgColor = black
     fps = 60
     initialWorld = ()
-    renderWorld  _ =  blank
-    handleWorld _ w  = w
+    renderWorld  _  =  blank
+    handleWorld _ w = w
     updateWorld _ w = w
     winSize = ( 800, 800 )
     winOffset = ( 100, 100 )
